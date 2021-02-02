e7ce924bbec1a83691758e8f878bf25d13996f06
Glenda <gsalas1@uco.edu>
2021-02-02 11:53:25 -0600

    fixed thread creation bugs

--- public/model/constant.js
+++ public/model/constant.js
@@ -1,3 +1,7 @@
 export const iDmodalSigninForm = 'modal-signin-form'
 export const iDmodalPopupInfo = 'modal-popupinfo'
-export const iDModalCreateNewThread = 'modal-create-thread-for'
+export const iDmodalCreateNewThread = 'modal-create-thread-form'
+
+export const collectionName = {
+    THREADS: 'threads',
+}
--- public/viewpage/util.js
+++ public/viewpage/util.js
@@ -1,13 +1,12 @@
 import * as Element from './element.js'
 import * as Constant from '../model/constant.js'
 
-
 export function popupInfo(title, body, modal){
     if(modal){
-        $('#' + modal).modal('hide')
+        $('#' +modal).modal('hide')
     }
 
     Element.popupInfoTitle.innerHTML = title
     Element.popupInfoBody.innerHTML = body
-    $('#' + Constant.iDmodalPopupInfo).modal('show')
+    $('#' +Constant.iDmodalPopupInfo).modal('show')
 }
